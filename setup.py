deb3492927a990923c9901b689ab3f738cbbec73
shailparmar03 <shailparmar26@gmail.com>
2025-04-11 15:14:44 +0530

    chore(version): bump version to 0.0.1
    Commit log:
    
    c3b8527 resolve(#1): Add command history.
    
    Signed-off-by: shailparmar03 <shailparmar26@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rahcomm",
-    version="0.0.0",
+    version="0.0.1",
     description="RAH Communication Tool for FPGA data communication on Vaaman.",
     author="Shail Parmar",
     author_email="shailparmar26@gmail.com.com",
